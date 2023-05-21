d5ab3425ef0348e7b43fa785fcb3fdbe3b44cc12
James Lawrence <jamdlaw@yahoo.com>
2023-05-20 20:56:25 -0700

    protected medication route with login

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,4 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::resource('medications', MedicationController::class);
+Route::resource('medications', MedicationController::class)->middleware('auth');;
